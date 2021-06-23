d07585ba4f75cf1dcec32f576d90b18484461245
maxim <maxim_sdrv@mail.ru>
2021-06-23 22:55:46 +0300

    nothing

--- Homework/main.cpp
+++ Homework/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+class Parent {
+protected:
+    int m_age;
+public:
+    explicit Parent(int age) : m_age(age) {}
+    virtual int getAge() {
+        return m_age;
+    }
+};
+
+class Child : public Parent {
+public:
+    explicit Child(int age) : Parent(age) {}
+    int getAge() override {
+        return m_age / 2;
+    }
+};
+
+void writeAge(Parent &someChild) {
+    std::cout << "rParent age " << someChild.getAge() << "\n";
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Child child(40);
+    Parent &rParent = child;
+    writeAge(rParent);
+
     return 0;
 }
